33e2667c5888f4721216b8c72dc8cd47346e06ac
cnsimo <cnsimo@163.com>
2021-05-28 17:39:00 +0800

    fix blazor base path.

--- src/SharpBlogX.Admin/Startup.cs
+++ src/SharpBlogX.Admin/Startup.cs
@@ -1,14 +1,19 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using Serilog;
+using SharpBlogX.Options;
 
 namespace SharpBlogX.Admin
 {
     public class Startup
     {
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IOptions<BlogOptions> blog)
         {
+            app.UseForwardedHeaders();
+            app.UsePathBase(new System.Uri(blog.Value.AdminUrl).PathAndQuery);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
